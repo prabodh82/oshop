fcb93f48f8f0f7dfca0074375e16f990ffea4bfb
prabodh82 <63994716+prabodh82@users.noreply.github.com>
2020-05-23 20:45:29 +0530

    Initial setup of oshop

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase :  { 
+    apiKey: "AIzaSyBL6akv92rF9RYBM0gj_j6utEnS5dqlJTM",
+    authDomain: "oshop-99888.firebaseapp.com",
+    databaseURL: "https://oshop-99888.firebaseio.com",
+    projectId: "oshop-99888",
+    storageBucket: "oshop-99888.appspot.com",
+    messagingSenderId: "228946805205",
+    appId: "1:228946805205:web:04ac73d77d5b969a4a7d5c"
+}
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase :  { 
+    apiKey: "AIzaSyBL6akv92rF9RYBM0gj_j6utEnS5dqlJTM",
+    authDomain: "oshop-99888.firebaseapp.com",
+    databaseURL: "https://oshop-99888.firebaseio.com",
+    projectId: "oshop-99888",
+    storageBucket: "oshop-99888.appspot.com",
+    messagingSenderId: "228946805205",
+    appId: "1:228946805205:web:04ac73d77d5b969a4a7d5c"
+}
 };
 
 /*
